5e897a27ab2e11ef0697fe753eeab6c04509a7f6
myamaan <yamaan.hanif777@gmail.com>
2025-03-05 05:13:10 +0500

    practice-pause

---
+++ app.js
@@ -0,0 +1,124 @@
+// IF…ELSE & ELSE IF STATEMENTS, TESTING SET OF CONDITIONS & SWITCH STATEMENTS
+// 1
+// var num = prompt ('enter number');
+// if (num % 3===0 ) {
+//     console.log('number is divisible by 3');
+// }
+// else {
+//     console.log('number is not divisible by 3');
+// }
+// // 2
+// var num = prompt ('enter number');
+// if (num % 2===0 ) {
+//     console.log('even numver');
+// }
+// else {
+//     console.log('odd number');
+// }
+// // 3
+// var age = prompt ('enter age');
+// if (age > 18 ) {
+//     console.log('Old enough');
+// }
+// else {
+//     console.log('Too young');
+// }
+// 4
+// var myName = 'yamaan';
+// var userName = prompt ('enter your name');
+// if (userName === myName) {
+//     console.log( 'welcome ' + userName + ' same name hai ree apna ');
+// }
+// 5
+//var num = prompt ('enter number');
+// if (num % 3===0 ) {
+//     console.log('number is divisible by 3');
+// }
+// else {
+//     console.log('number is not divisible by 3');
+// }
+
+// 6
+
+
+// 7
+// var num1 = +prompt ('enter first number');
+// var num2 = +prompt ('enter second number');
+// var num3 = prompt ('select operater , + , - , / , * , %');
+// if (num3 === '+') {
+//     console.log(num1 + num2);
+// }
+// else if (num3 === '-') {
+//     console.log(num1 - num2);
+// }
+// else if (num3 === '/') {
+//     console.log(num1 / num2);
+// }
+// else if (num3 === '*') {
+//     console.log(num1 * num2);
+// }
+// else if (num3 === '%') {
+//     console.log(num1 % num2);
+// }
+// else {
+//     console.log('galat hai bhai');
+// }
+// 8
+// var time = prompt ('enter time in  24 hours clock format like: 1900 = 7pm.')
+// if (time>= 0 && time <1200 ) {
+// console.log('Good morning');
+// }
+// else if (time>= 1200 && time <1700 ) {
+//     console.log('Good afternoon')
+// }
+// else if (time>= 1700 && time <2100 ) {
+// console.log('Good evening');
+// }
+// else if (time>= 2100 && time <2359 ) {
+// console.log('Good night');
+// }
+// else {
+//     console.log('bhai sahi time nahi dala hai');
+// }
+// 9
+
+// 10 
+// var pass1 = prompt ('enter password');
+// var pass2 = prompt ('re-enter password');
+// if (pass1 === pass2) (
+//     console.log('Correct password')
+// )
+// else {
+//     console.log('incorrect password')
+// }
+// 11 
+// var firstName = prompt ('enter your name');
+// if (firstName === 'fahad'){
+//     console.log('hello fahad');
+// }
+// else {
+//     console.log('you are not fahad');
+// }
+// 12 
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) { 
+// greeting = "Good day"; 
+// }
+// else {
+// greeting = "Good evening"; 
+// } 
+//16
+// var num = prompt ('enter variable type');
+// console.log((typeof num));
+
+// 18 
+// console.log(10 !== 8 );
+// 20
+// var age = prompt ('enter age');
+// if (age > 18 ) {
+//     console.log('Old enough');
+// }
+// else {
+//     console.log('Too young');
+// }
